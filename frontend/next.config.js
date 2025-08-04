cc98c1a8a258e8b729d0bcf078a70d053f0353d5
Tetsuya Hamaki <tetsuya.hamaki@mims.com>
2025-08-05 07:24:25 +0800

    frontend: replace JSON view with table UI

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/v0/:path*'
+      }
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
